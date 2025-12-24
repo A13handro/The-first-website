5c4143697903cf3d1ba4f4655573f883decc0994
A13handro <alexsandr.07k@gmail.com>
2025-12-24 22:37:44 +0300

    Добавил отображение постов

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
 
@@ -9,13 +10,41 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type Article struct {
+	Id                     uint16
+	Title, Anons, FullText string
+}
+
+var posts = []Article{}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		panic(err)
 	}
 
-	t.Execute(w, "index.html")
+	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	table, err := db.Query("SELECT * FROM articles")
+	if err != nil {
+		panic(err)
+	}
+	posts = []Article{}
+	for table.Next() {
+		var post Article
+		err = table.Scan(&post.Id, &post.Title, &post.Anons, &post.FullText)
+		if err != nil {
+			panic(err)
+		}
+		posts = append(posts, post)
+	}
+
+	t.ExecuteTemplate(w, "index", posts)
 }
 func handlfunc() {
 	rtr := mux.NewRouter()
