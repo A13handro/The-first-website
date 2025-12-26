d4062c950f40de10e1a6067f548160353825126e
A13handro <alexsandr.07k@gmail.com>
2025-12-26 15:38:11 +0300

    Сделал полностью рабочий обязательный вход на сайт через токены (Проверка токенов очень колхозная, я не разобрался как передавать в заголовок)

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"database/sql"
+	"encoding/hex"
+	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
-
 	_ "github.com/lib/pq"
 )
 
@@ -15,13 +19,50 @@ type Article struct {
 	Title, Anons, FullText string
 }
 
-type Mes struct {
-	Message string
-}
-
-var data = Mes{}
 var posts = []Article{}
 var showPosts = Article{}
+
+var ThisToken string
+var ThiseID int
+var Message string
+var secretKey = []byte("my_secret_key")
+
+func generateToken(userID int) (string, error) {
+	claims := jwt.MapClaims{
+		"id":  userID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
+func parseToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+}
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		var authToken string
+		erro := db.QueryRow("SELECT auth_token FROM users WHERE id = $1", ThiseID).Scan(&authToken)
+		if erro != nil {
+			Message = "Токен не получен!"
+			http.Redirect(w, r, "/authorization", http.StatusSeeOther)
+		}
+		if authToken == ThisToken {
+			next(w, r)
+		} else {
+			fmt.Println("FFFFFFFFFf")
+		}
+	}
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/header.html", "templates/index.html", "templates/footer.html")
@@ -58,7 +99,51 @@ func authorization(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	t.Execute(w, "authorization")
+	t.ExecuteTemplate(w, "authorization", Message)
+	Message = ""
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	login := r.FormValue("login")
+	password := r.FormValue("password")
+	if login == "" || password == "" {
+		Message = "Не все поля заполнены!"
+		http.Redirect(w, r, "/authorization", http.StatusSeeOther)
+		return
+	}
+	hash := md5.Sum([]byte(password))
+	hashedPass := hex.EncodeToString(hash[:])
+
+	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	insert, err := db.Query("SELECT id FROM users WHERE login = $1 AND hashed_password = $2", login, hashedPass)
+	if err != nil {
+		fmt.Println("НЕТ!")
+		panic(err)
+	}
+	if insert.Next() {
+		var id int
+		err := insert.Scan(&id)
+		if err != nil {
+			panic(err)
+		}
+		ThiseID = id
+		ThisToken, _ = generateToken(id)
+		_, err = db.Exec("UPDATE users SET auth_token = $1 WHERE id = $2", ThisToken, id)
+		if err != nil {
+			panic(err)
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	} else {
+		Message = "Неверный логин или пароль!"
+		http.Redirect(w, r, "/authorization", http.StatusSeeOther)
+	}
+	insert.Close()
 }
 
 func registration(w http.ResponseWriter, r *http.Request) {
@@ -75,8 +160,8 @@ func create(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	t.ExecuteTemplate(w, "create", data)
-	data = Mes{""}
+	t.ExecuteTemplate(w, "create", Message)
+	Message = ""
 }
 
 func save_article(w http.ResponseWriter, r *http.Request) {
@@ -85,30 +170,31 @@ func save_article(w http.ResponseWriter, r *http.Request) {
 	full_text := r.FormValue("full_text")
 
 	if title == "" || anons == "" || full_text == "" {
-		data = Mes{"Не все поля заполнены!"}
+		Message = "Не все поля заполнены!"
 		http.Redirect(w, r, "/create", http.StatusSeeOther)
-	} else {
-		connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
-		db, err := sql.Open("postgres", connStr)
-		if err != nil {
-			panic(err)
-		}
-		defer db.Close()
-
-		err = db.Ping()
-		if err != nil {
-			panic(err)
-		}
-
-		insert, err := db.Query("INSERT INTO articles (title, anons, full_text) "+
-			"VALUES ($1, $2, $3)", title, anons, full_text)
-		if err != nil {
-			panic(err)
-		}
-		insert.Close()
-
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
+	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	insert, err := db.Query("INSERT INTO articles (title, anons, full_text) "+
+		"VALUES ($1, $2, $3)", title, anons, full_text)
+	if err != nil {
+		panic(err)
+	}
+	insert.Close()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
 }
 
 func show_post(w http.ResponseWriter, r *http.Request) {
@@ -167,13 +253,14 @@ func delete_post(w http.ResponseWriter, r *http.Request) {
 func handlfunc() {
 	rtr := mux.NewRouter()
 
-	rtr.HandleFunc("/", index).Methods("GET")
-	rtr.HandleFunc("/create", create).Methods("GET")
+	rtr.HandleFunc("/", authMiddleware(index)).Methods("GET")
+	rtr.HandleFunc("/create", authMiddleware(create)).Methods("GET")
 	rtr.HandleFunc("/save_article", save_article).Methods("POST")
-	rtr.HandleFunc("/post/{id:[0-9]+}", show_post).Methods("GET")
-	rtr.HandleFunc("/delete_post/{id:[0-9]+}", delete_post).Methods("GET")
+	rtr.HandleFunc("/post/{id:[0-9]+}", authMiddleware(show_post)).Methods("GET")
+	rtr.HandleFunc("/delete_post/{id:[0-9]+}", authMiddleware(delete_post)).Methods("GET")
 	rtr.HandleFunc("/authorization", authorization).Methods("GET")
 	rtr.HandleFunc("/registration", registration).Methods("GET")
+	rtr.HandleFunc("/auth", auth).Methods("POST")
 
 	http.Handle("/", rtr)
 
