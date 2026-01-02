68a1ceeae0f806b3f430bf004cd223d7b9530ad3
A13handro <alexsandr.07k@gmail.com>
2026-01-02 17:13:09 +0400

    Почистил код, написал комментари, изменил метод хеширования на bcrypt

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require github.com/lib/pq v1.10.9
 require github.com/golang-jwt/jwt/v5 v5.3.0
 
 require github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+
+require golang.org/x/crypto v0.46.0
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"crypto/md5"
 	"database/sql"
 	"encoding/base64"
-	"encoding/hex"
 	"fmt"
 	"html/template"
 	"io"
@@ -17,6 +15,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Article struct {
@@ -35,6 +34,7 @@ var RefreshToken string
 
 var ThiseID int
 var Message string
+
 var secretKey = []byte("my_secret_key")
 
 func checkErr(err error) { //Проверка на ошибку
@@ -54,40 +54,41 @@ func generateToken(userID int, num int) (string, error) { //Создание т�
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secretKey)
 }
-func parseToken(tokenString string) (*jwt.Token, error) { //Проверка токена
+func parseToken(tokenString string) (*jwt.Token, error) { //Проверка токена на действительность
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return secretKey, nil
 	})
 }
-func authMiddleware(next http.HandlerFunc) http.HandlerFunc { //Проверка токена
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc { //Основная проверка токена
 	return func(w http.ResponseWriter, r *http.Request) {
 		connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
 		db, err := sql.Open("postgres", connStr)
 		checkErr(err)
 		defer db.Close()
 
-		var authTokenA string
-		var authTokenR string
+		var authTokenA string //Переменная для коротковременного токена в бд
+		var authTokenR string //Переменная для долговременного токена в бд
+
 		table, err := db.Query("SELECT accesstoken FROM users WHERE id = $1", ThiseID)
 		checkErr(err)
 		if table.Next() {
 			table.Scan(&authTokenA)
 		}
-		if AccessToken == "" || AccessToken != authTokenA {
+		if AccessToken == "" || AccessToken != authTokenA { //Если нет сохраненного глобально токена
 			Message = "Пожалуйста, авторизируйтесь!"
 			http.Redirect(w, r, "/api/auth/login", http.StatusSeeOther)
 			return
 		}
-		tokenA, _ := parseToken(authTokenA)
+		tokenA, _ := parseToken(authTokenA) //Проверяем на действительность токен
 		if !tokenA.Valid {
 			err1 := db.QueryRow("SELECT refreshtoken FROM users WHERE id = $1", ThiseID).Scan(&authTokenR)
-			checkErr(err1)
-			tokenR, _ := parseToken(authTokenR)
-			if tokenR.Valid {
+			checkErr(err1)                      //Вытягиваем refreshtoken
+			tokenR, _ := parseToken(authTokenR) //Проверяем
+			if tokenR.Valid {                   //Обновляем AccessToken, если refreshtoken действителен
 				AccessToken, _ = generateToken(ThiseID, 15)
 				_, err = db.Exec("UPDATE users SET accesstoken = $1 WHERE id = $2", AccessToken, ThiseID)
 				checkErr(err)
-			} else {
+			} else { //Иначе
 				Message = "Время сеанса истекло. Пожалуйста, авторезируйтесь повторно."
 				http.Redirect(w, r, "/api/auth/login", http.StatusSeeOther)
 				return
@@ -97,7 +98,7 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc { //Проверка
 	}
 }
 
-func index(w http.ResponseWriter, r *http.Request) { //Главная страница
+func index(w http.ResponseWriter, r *http.Request) { //Главная страница (GET)
 	t, err := template.ParseFiles("templates/header.html", "templates/index.html", "templates/footer.html")
 	checkErr(err)
 
@@ -120,7 +121,7 @@ func index(w http.ResponseWriter, r *http.Request) { //Главная стран
 		post.Rol = role
 		Art = append(Art, post)
 	}
-	data := struct {
+	data := struct { //Создаем структуру для передачи данных в html
 		Posts []Article
 		Role  bool
 		Mes   string
@@ -133,7 +134,7 @@ func index(w http.ResponseWriter, r *http.Request) { //Главная стран
 	Message = ""
 }
 
-func login(w http.ResponseWriter, r *http.Request) { //Авторизация
+func login(w http.ResponseWriter, r *http.Request) { //Авторизация (GET)
 	t, err := template.ParseFiles("templates/header.html", "templates/login.html")
 	checkErr(err)
 
@@ -141,44 +142,49 @@ func login(w http.ResponseWriter, r *http.Request) { //Авторизация
 	RefreshToken = ""
 
 	t.ExecuteTemplate(w, "login", Message)
-	Message = ""
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
-func log(w http.ResponseWriter, r *http.Request) { //обработка Авторизации
+func log(w http.ResponseWriter, r *http.Request) { //обработка Авторизации (POST)
 	email := r.FormValue("email")
 	password := r.FormValue("password")
-	if email == "" || password == "" {
+	if email == "" || password == "" { //Проверка на заполнение формы
 		Message = "Не все поля заполнены!"
 		http.Redirect(w, r, "/api/auth/login", http.StatusSeeOther)
 		return
 	}
-	hash := md5.Sum([]byte(password))
-	hashedPass := hex.EncodeToString(hash[:])
 
 	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	checkErr(err)
 	defer db.Close()
 
-	insert, err := db.Query("SELECT id FROM users WHERE email = $1 AND passwordhash = $2", email, hashedPass)
-	checkErr(err)
-	if insert.Next() {
-		var id int
-		err := insert.Scan(&id)
-		checkErr(err)
-		ThiseID = id
-		AccessToken, _ = generateToken(id, 2)
-		RefreshToken, _ = generateToken(id, 168)
-		_, err = db.Exec("UPDATE users SET accesstoken = $1, refreshtoken = $2 WHERE id = $3", AccessToken, RefreshToken, id)
-		checkErr(err)
-		http.Redirect(w, r, "/api", http.StatusSeeOther)
-	} else {
-		Message = "Неверный логин или пароль!"
+	var storedHash string
+	var id int
+	err = db.QueryRow("SELECT passwordhash, id FROM users WHERE email = $1", email).Scan(&storedHash, &id) //Поиск
+	if err != nil {
+		Message = "Неверный логин"
 		http.Redirect(w, r, "/api/auth/login", http.StatusSeeOther)
+		return
 	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storedHash), []byte(password)) //Сравение пароля
+	if err != nil {
+		Message = "Неверный пароль"
+		http.Redirect(w, r, "/api/auth/login", http.StatusSeeOther)
+		return
+	}
+
+	ThiseID = id                             // Запоминаем ID пользователя
+	AccessToken, _ = generateToken(id, 2)    // Создаем токен на 2 часа
+	RefreshToken, _ = generateToken(id, 168) // Создаем токен на неделю
+	_, err = db.Exec("UPDATE users SET accesstoken = $1, refreshtoken = $2 WHERE id = $3", AccessToken, RefreshToken, id)
+	checkErr(err) //Отправляем токен в бд
+	http.Redirect(w, r, "/api", http.StatusSeeOther)
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
-func register(w http.ResponseWriter, r *http.Request) { //Регистрация
+func register(w http.ResponseWriter, r *http.Request) { //Регистрация (GET)
 	t, err := template.ParseFiles("templates/header.html", "templates/register.html")
 	checkErr(err)
 
@@ -186,17 +192,29 @@ func register(w http.ResponseWriter, r *http.Request) { //Регистрация
 	RefreshToken = ""
 
 	t.ExecuteTemplate(w, "register", Message)
-	Message = ""
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
-func reg(w http.ResponseWriter, r *http.Request) { //обработка Регистрации
+func reg(w http.ResponseWriter, r *http.Request) { //обработка Регистрации (POST)
 	email := r.FormValue("email")
-	em := govalidator.IsEmail(strings.TrimSpace(email))
+	em := govalidator.IsEmail(strings.TrimSpace(email)) //Проверка на форму email
 	password := r.FormValue("password")
-	name := r.FormValue("name")
-	surname := r.FormValue("surname")
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost) //Хешируем
+	checkErr(err)
+	fmt.Println(hashedPass)
+
+	name := r.FormValue("name")       //Имя
+	surname := r.FormValue("surname") //Фамилия
+
 	roleStr := r.FormValue("role")
-	if email == "" || password == "" || name == "" || surname == "" {
+	var author bool
+	if roleStr == "true" { //Перевод из строки в bool
+		author = true
+	} else {
+		author = false
+	}
+
+	if email == "" || password == "" || name == "" || surname == "" { //Проверка на заполнение формы
 		Message = "Не все поля заполнены!"
 		http.Redirect(w, r, "/api/auth/register", http.StatusSeeOther)
 		return
@@ -205,41 +223,27 @@ func reg(w http.ResponseWriter, r *http.Request) { //обработка Реги
 		http.Redirect(w, r, "/api/auth/register", http.StatusSeeOther)
 		return
 	}
-	hash := md5.Sum([]byte(password))
-	hashedPass := hex.EncodeToString(hash[:])
-
-	var author bool
-	if roleStr == "true" {
-		author = true
-	} else {
-		author = false
-	}
-	fmt.Println(author)
 
 	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err)
 	defer db.Close()
 
 	var idd string
 	erro := db.QueryRow("SELECT id FROM users WHERE email = $1", email).Scan(&idd)
-	if erro == nil {
-		Message = "Такой email уже существует!"
+	if erro == nil { //Проверка на уникальность email
+		Message = "Такой email уже зарегистрирован!"
 		http.Redirect(w, r, "/api/auth/register", http.StatusSeeOther)
 		return
 	}
 	insert, err := db.Query("INSERT INTO users (email, passwordhash, name, surname, role) "+
 		"VALUES ($1, $2, $3, $4, $5)", email, hashedPass, name, surname, author)
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err) //Сохраняем в бд
 	insert.Close()
 	http.Redirect(w, r, "/api/auth/login", http.StatusSeeOther)
 }
 
-func posts(w http.ResponseWriter, r *http.Request) { //Создание поста
+func posts(w http.ResponseWriter, r *http.Request) { //Создание поста (GET)
 	t, err := template.ParseFiles("templates/header.html", "templates/posts.html", "templates/footer.html")
 	checkErr(err)
 
@@ -258,33 +262,31 @@ func posts(w http.ResponseWriter, r *http.Request) { //Создание пост
 	}
 
 	t.ExecuteTemplate(w, "posts", Message)
-	Message = ""
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
-func pos(w http.ResponseWriter, r *http.Request) { //обработка создания или изменения поста
-	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable" //Открываем бд
+func pos(w http.ResponseWriter, r *http.Request) { //обработка создания или изменения поста (POST)
+	connStr := "user=postgres password=123 port=5432 dbname=usersdb sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err)
 	defer db.Close()
 
-	flag := r.FormValue("pict")       //Галочка, что картинки не будет
-	title := r.FormValue("title")     //название статьи
+	flag := r.FormValue("pict")       //ФЛАЖОК, что картинки не будет
+	title := r.FormValue("title")     //Название статьи
 	content := r.FormValue("content") //Текст статьи
 	idS := r.FormValue("id")          //ID
 	id, _ := strconv.Atoi(idS)
 	adres := r.FormValue("a")          //Адрес страницы, откуда вызвалась функция
 	file, _, err := r.FormFile("file") //Файл картинки
 
-	var pictur bool = false
+	var pictur bool = false       //Здесь начинается сложная логика
 	if flag == "" && err != nil { //НЕТ КАРТИНКИ и ФЛАЖКА
 		pictur = true
 	}
 
-	if title == "" || content == "" || (pictur && adres == "/api/posts") {
+	if title == "" || content == "" || (pictur && adres == "/api/posts") { //Проверка на заполнение формы
 		Message = "Не все поля заполнены!"
-		if adres != "/api/posts" {
+		if adres != "/api/posts" { //Отправляем обратно на ту же страницу
 			http.Redirect(w, r, adres, http.StatusSeeOther)
 			return
 		}
@@ -293,34 +295,35 @@ func pos(w http.ResponseWriter, r *http.Request) { //обработка созд
 	}
 
 	var fileCod string
-	var fileBytes = []byte("s")
+	var fileBytes = []byte("s")        // Это значение будет обозначать отсутствие картинки
 	if pictur == false && flag == "" { //Если есть картинка
 		fileBytes, err = io.ReadAll(file)
 		checkErr(err)
 		defer file.Close()
 	}
-	fileCod = base64.StdEncoding.EncodeToString(fileBytes)
+	fileCod = base64.StdEncoding.EncodeToString(fileBytes) //Кодируем для чтения в html
 
 	ins, err3 := db.Query("SELECT * FROM articles WHERE id = $1", id)
 	checkErr(err3)
-	if ins.Next() { // Если редактируем пост
-		if pictur == true {
+	if ins.Next() { // Если пост редактируется
+		if pictur == true { //Картинка не меняется
 			_, err = db.Exec("UPDATE articles SET title = $1, content = $2 WHERE id = $3", title, content, id)
-		} else {
+		} else { //Картинка обновляется
 			_, err = db.Exec("UPDATE articles SET title = $1, content = $2, data = $3 WHERE id = $4", title, content, fileCod, id)
 		}
 		checkErr(err)
-	} else {
+	} else { //Иначе просто создаем пост
 		insert, err := db.Query("INSERT INTO articles (title, content, data) VALUES ($1, $2, $3)", title, content, fileCod)
 		checkErr(err)
 		insert.Close()
 	}
 
 	http.Redirect(w, r, "/api", http.StatusSeeOther)
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
-func edit_post(w http.ResponseWriter, r *http.Request) { //Редактирование поста
-	vars := mux.Vars(r)
+func edit_post(w http.ResponseWriter, r *http.Request) { //Редактирование поста (GET)
+	vars := mux.Vars(r) //Принимаем ID поста
 
 	t, err := template.ParseFiles("templates/header.html", "templates/show.html", "templates/footer.html")
 	checkErr(err)
@@ -342,7 +345,7 @@ func edit_post(w http.ResponseWriter, r *http.Request) { //Редактиров�
 		return
 	}
 	showPosts = Article{}
-	for insert.Next() {
+	for insert.Next() { //Читаем из бд каждый пост
 		var post Article
 		err = insert.Scan(&post.Id, &post.Title, &post.Content, &post.Data)
 		post.Mes = Message
@@ -351,6 +354,7 @@ func edit_post(w http.ResponseWriter, r *http.Request) { //Редактиров�
 	}
 
 	t.ExecuteTemplate(w, "show", showPosts)
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
 func delete_post(w http.ResponseWriter, r *http.Request) { //Удаление поста
@@ -375,6 +379,7 @@ func delete_post(w http.ResponseWriter, r *http.Request) { //Удаление п
 	insert.Close()
 
 	http.Redirect(w, r, "/api", http.StatusSeeOther)
+	Message = "" //Обнуляем сообщение после передачи пользователю
 }
 
 func handlfunc() {
@@ -391,7 +396,6 @@ func handlfunc() {
 	rtr.HandleFunc("/api/auth/reg", reg).Methods("POST")                                   //обработка Регистрации
 
 	http.Handle("/", rtr)
-
 	http.ListenAndServe(":8080", nil)
 }
 
